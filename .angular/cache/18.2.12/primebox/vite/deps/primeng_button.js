b3410a825c2ec4385e72456bc88c841f786be419
abraham.villca <vicovillca@hotmail.com>
2026-01-09 19:43:22 -0400

    Auto-generated commit

---
+++ .angular/cache/18.2.12/primebox/vite/deps/primeng_button.js
@@ -0,0 +1,26 @@
+import {
+  Button,
+  ButtonClasses,
+  ButtonDirective,
+  ButtonIcon,
+  ButtonLabel,
+  ButtonModule,
+  ButtonStyle
+} from "./chunk-2WXK5TK3.js";
+import "./chunk-SQVOO4GY.js";
+import "./chunk-JLHWEZCQ.js";
+import "./chunk-IBOJPABQ.js";
+import "./chunk-CTMS2UJW.js";
+import "./chunk-5UDK7ABK.js";
+import "./chunk-SLFMBYBS.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  Button,
+  ButtonClasses,
+  ButtonDirective,
+  ButtonIcon,
+  ButtonLabel,
+  ButtonModule,
+  ButtonStyle
+};
+//# sourceMappingURL=primeng_button.js.map
